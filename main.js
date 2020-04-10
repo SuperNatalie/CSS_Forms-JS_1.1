bd88645670e26ad4a1ef357cf09a2e4d79019e55
Natalie <natalie@MacBook-Pro.local>
2020-04-10 23:10:01 +0200

    ready

---
+++ main.js
@@ -0,0 +1,10 @@
+
+
+function addition() {
+var a = document.getElementById("number").value;
+var b = 2;
+var c = +a + b;
+document.getElementById("ergebnis").innerText = c;
+}
+
+// Forms können nur Texte, deshalb muss man var a in zahl umwandeln mit dem + davor
